8c14a2a296f4840165323855464027fc94c244cb
hddgpp <youssefhehe3@gmail.com>
2025-11-10 14:25:12 +0100

    Add Chat component to App

--- Chatbot/src/App.jsx
+++ Chatbot/src/App.jsx
@@ -1,9 +1,11 @@
+import Chat from "./components/Chat/Chat.jsx";
 import InputText from "./components/Input-text/Input-text.jsx";
 
 export default function App() {
   return(
     <>
      <InputText/>
+     <Chat/>
     </>
   )
 }
