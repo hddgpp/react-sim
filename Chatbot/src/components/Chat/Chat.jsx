295c9576e45e7f50ef0b9f8f2495b89d5d8ca6b2
hddgpp <youssefhehe3@gmail.com>
2025-11-11 03:32:40 +0100

    Refactor icon rendering logic in Chat component for improved clarity

--- Chatbot/src/components/Chat/Chat.jsx
+++ Chatbot/src/components/Chat/Chat.jsx
@@ -2,23 +2,15 @@ import React from 'react'
 
 export default function Chat({sender, message}) {
 
-    let icon = '../../../public/user.png'
-
-    if(sender === 'robot') {
-        icon = '../../../public/robot.png'
-        return (
-            <div>
-                <img src={icon} width={50} alt="" />
-                {message}
-            </div>
-        )
-    }
+    const userIcon = '../../../public/user.png'
+    const robotIcon = '../../../public/robot.png'
 
     return(
         <main>
             <div>
+                {sender === 'robot' && <img src={robotIcon} width={50} alt="" />}
                 {message}
-               <img src={icon} width={50} alt="" />
+               <img src={userIcon} width={50} alt="" />
             </div>
         </main>
     )
