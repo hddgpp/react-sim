170a29326213edbef86b8266b74dd2f8c93d093c
hddgpp <youssefhehe3@gmail.com>
2025-11-11 03:01:20 +0100

    Pass message prop to Chat component and render it

--- Chatbot/src/components/Chat/Chat.jsx
+++ Chatbot/src/components/Chat/Chat.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import user from '../../../public/user.png'
 
-export default function Chat() {
+export default function Chat(props) {
+
+    const message = props.message
+
     return(
         <main>
             <div>
+                {message}
                <img src={user} width={50} alt="" />
             </div>
         </main>
