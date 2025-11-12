af485908eaa0b35603b15c672adbced3ba31125d
hddgpp <youssefhehe3@gmail.com>
2025-11-12 21:01:53 +0100

    Refactor Chat component to apply conditional styling for user and robot message containers

--- Chatbot/src/components/Chat/Chat.jsx
+++ Chatbot/src/components/Chat/Chat.jsx
@@ -8,7 +8,7 @@ export default function Chat({sender, message}) {
 
     return(
         <main>
-            <div>
+            <div className={sender === 'user' ? 'user-container' : 'robot-container'}>
                 {sender === 'robot' && <img src={robotIcon} width={50} alt="" />}
                 {message}
                 {sender === 'user' && <img src={userIcon} width={50} alt="" />}
