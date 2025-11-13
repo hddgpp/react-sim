ee3d2d5193a8e43707286c54b175bc65427fe30d
hddgpp <youssefhehe3@gmail.com>
2025-11-13 17:44:51 +0100

    Pressing Enter in the input will active the inputText logic

--- Chatbot/src/components/Input-text/Input-text.jsx
+++ Chatbot/src/components/Input-text/Input-text.jsx
@@ -1,53 +1,55 @@
 import React from 'react'
 import './Input-text.css'
 
-export default function InputText({setSubmit}) {
+export default function InputText({ setSubmit }) {
+  const [inputText, setInputText] = React.useState('')
 
-    const [inputText, setInputText] = React.useState('')
+  function handleChange(event) {
+    setInputText(event.target.value)
+  }
 
-    function handleChange(event) {
-        setInputText((event.target.value))
+  function sendMessage() {
+    if (inputText.trim() === '') return
+
+    const response = window.Chatbot.getResponse(inputText)
+
+    setSubmit(prev => [
+      ...prev,
+      {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID()
+      },
+      {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ])
+
+    setInputText('')
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      sendMessage()
     }
+  }
 
-    function sendMessage() {
-
-        if (inputText.trim() === "") return; 
-
-        const response = window.Chatbot.getResponse(inputText)
-
-        setSubmit(prev => [
-            ...prev,
-            {
-                message: inputText,
-                sender: 'user',
-                id: crypto.randomUUID()
-            },
-            {
-                message: response,
-                sender: 'robot',
-                id: crypto.randomUUID()
-            }
-        ])
-        
-
-         setInputText("");
-    }
-
-    return (
-        
-            <div className='container'>
-                
-                    <input 
-                        type="text"
-                        className='textInput'
-                        placeholder='Ask anything'
-                        value={inputText}
-                        onChange={handleChange}
-                        />
-                <button 
-                    className='sendBtn'
-                    onClick={sendMessage}
-                >Send</button>
-            </div>
-    )
+  return (
+    <div className='container'>
+      <input
+        type='text'
+        className='textInput'
+        placeholder='Ask anything'
+        value={inputText}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button className='sendBtn' onClick={sendMessage}>
+        Send
+      </button>
+    </div>
+  )
 }
